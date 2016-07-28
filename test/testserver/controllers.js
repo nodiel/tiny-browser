742ad6326d549e5e7996a6727ecc5df5fc656c7a
Nodiel Clavijo <nodiel@citizen.co>
2016-07-28 15:49:49 -0400

    Added new view to the testserver

--- test/testserver/controllers.js
+++ test/testserver/controllers.js
@@ -2,11 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/test-click', function(request, response) {
-    response.render('test-click');    
+    response.render('test-click');
 });
 
 router.get('/test-waitfor', function(request, response) {
     response.render('waitfor');
+});
+
+router.get('/', function(request, response) {
+    response.render('home');
 });
 
 module.exports = router;
