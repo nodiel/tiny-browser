64dbe91f5e0676695bbf25e2c6dcd2fac6d41cf2
Nodiel Clavijo <nodiel@citizen.co>
2016-07-28 15:51:18 -0400

    Added unit testing for the `evaluate` method

--- test/test.js
+++ test/test.js
@@ -59,4 +59,49 @@ describe('TinyBrowser', function() {
                 });
         });
     });
+
+    describe('#evaluate()', function() {
+        it('should successfully evaluate the function in the remote webpage.', function() {
+            var browserInstance = null;
+
+            return browser.create()
+                .then(function(instance) {
+                    browserInstance = instance;
+                    return browserInstance.open('http://localhost:3000/test-click/')
+                })
+                .then(function() {
+                    return browserInstance.click('#click-here');
+                })
+                .then(function() {
+                    return browserInstance.evaluate(function() {
+                        var label = document.querySelector('#message');
+
+                        if (label) {
+                            return label.innerText;
+                        }
+
+                        return 'ERROR';
+                    });
+                })
+                should.eventually.equal('clicked button');
+        });
+    });
+
+    describe('#evaluate()', function() {
+        it('should evaluate the passed function with the paramaters passed as well', function() {
+            var browserInstance = null;
+
+            return browser.create()
+                .then(function(instance) {
+                    browserInstance = instance;
+                    return browserInstance.open('http://localhost:3000/');
+                })
+                .then(function() {
+                    return browserInstance.evaluate(function(selector) {
+                        return document.querySelector(selector).innerText;
+                    }, '#header');
+                })
+                .should.eventually.equal('Some header');
+        });
+    });
 });
